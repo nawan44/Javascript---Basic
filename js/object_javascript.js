a513ebf8791806791c73ea2f209cf4065199a2fa
nawan44 <rachmat.d.gunawan@gmail.com>
2020-06-07 22:12:07 +0700

    Basic Object Javascript

---
+++ js/object_javascript.js
@@ -0,0 +1,15 @@
+var foo = {};
+console.log(typeof foo);     //object
+
+var mahasiswa = {
+    nama : "Rachmat ",
+    jurusan : "Teknik Informatika",
+    ipk   : 3.79,
+    keterangan : "LULUS"
+};
+
+
+console.log( mahasiswa.nama );         //Rachmat
+console.log( mahasiswa.jurusan );      //Teknik Informatika
+console.log( mahasiswa.ipk );          //3.79
+console.log( mahasiswa.keterangan );   //LULUS
