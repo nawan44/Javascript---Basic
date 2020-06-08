ab4afc09ed6f818083b6888e4bbf4fd2e16de006
nawan44 <rachmat.d.gunawan@gmail.com>
2020-06-08 22:44:14 +0700

    Object Property

---
+++ js/looping_array.js
@@ -0,0 +1,8 @@
+var nama = ["Aba", "Bada", "Cici", "Dia", "Eka"];
+var jumlah_nama = nama.length;
+
+console.log("Jumlah nama = " + jumlah_nama);
+
+for(var i = 0; i < jumlah_nama; i++){
+    console.log(nama[i]);
+}
--- js/object_javascript.js
+++ js/object_javascript.js
@@ -1,15 +1,74 @@
 var foo = {};
 console.log(typeof foo);     //object
 
-var mahasiswa = {
-    nama : "Rachmat ",
-    jurusan : "Teknik Informatika",
-    ipk   : 3.79,
-    keterangan : "LULUS"
+var student = {
+    name : "Rachmat ",
+    faculty : "Informatics",
+    GPA   : 3.79,
+    evidence : "PASS"
 };
 
 
-console.log( mahasiswa.nama );         //Rachmat
-console.log( mahasiswa.jurusan );      //Teknik Informatika
-console.log( mahasiswa.ipk );          //3.79
-console.log( mahasiswa.keterangan );   //LULUS
+console.log(student.name );         //Rachmat
+console.log(student.faculty );      //Informatics
+console.log(student["gpa"]);        //*UNDEFINED*
+console.log(student["evidence"]);   //PASS
+
+var student1={
+    "your fullname" : "Rachmat Gunawan",
+    "your faculty" : "Informatics",
+    "your gpa" : "3,79",
+    "your evidence" : "PASS cumlaude"
+};
+
+// console.log(student. name lengkap);   // SyntaxError: missing ) after argument list
+console.log(student1["your fullname"]);
+console.log(student1["your faculty"]);
+console.log(student1["your gpa"]);
+
+var absent = {  1: "Aan",
+                "2": "Beta",
+                3: "Cici",
+                "4": "Dian",
+                "5" : "koh"
+}
+
+console.log(absent[1]);
+console.log(absent[2]);
+console.log(absent["3"]);
+console.log(absent["4"]);
+// console.log(absent.5);// SyntaxError: missing ) after argument list
+
+
+//ADD OBJECT PROPERTY
+
+var student2 = {};
+student2.name = "andi";
+student2.faculty = "Biology";
+console.log(student2.name);   //andi
+
+var student3 = {};
+student3["your name"] = "Rachmat Gunawan";
+student3["your faculty"] = "Archeology";
+console.log(student3["your name"]);    //Rachmat Gunawan
+
+//CHANGE OBJECT PROPERTY
+
+student2["your name"] = "Ruhendi Saputra";
+console.log(student2["your name"]);    //Ruhendi Saputra
+
+//OBJECT METHOD
+
+var car = {
+    merk : "Toyota Innova",
+    tipe : "MPV",
+    price : 2000000000,
+    color : "blue",
+    starton : function (){return "Engine On"},
+    goon : function(placex){ return "GOes To"}
+};
+
+console.log(car.merk);     // Toyota Innove
+console.log(car["tipe"]);  // MPV
+console.log(car.starton());
+console.log(car["goon"]("Marise"));
