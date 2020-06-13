105cc51da5f7b1aace83396dadd7554f00e25984
nawan44 <rachmat.d.gunawan@gmail.com>
2020-06-13 21:27:28 +0700

    oop this

--- js/oop_javascrpt.js
+++ js/oop_javascrpt.js
@@ -5,3 +5,19 @@ console.log(kai);
 var say = new String ("hello indo");
 var kar = say.trim(6, 2);
 console.log(kar);
+
+
+class Car{
+    constructor(myMerk, myType, myPrice ){
+        this.merk = myMerk;
+        this.type = myType;
+        this.price = myPrice;
+    }
+}
+var carAndi = new Car( "Toyota Innova", "MPV", 175000000);
+console.log(carAndi.merk);
+console.log(carAndi.price);
+
+var carJoko = new Car( "Audi A8", "Sport", 455000000);
+console.log(carJoko.merk);
+console.log(carJoko.price);
