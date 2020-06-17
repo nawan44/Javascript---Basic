ebadb83de367cb5c6ec3b614137fd5799bf85a7a
nawan44 <rachmat.d.gunawan@gmail.com>
2020-06-17 22:17:58 +0700

    Class Property

--- js/oop_javascrpt.js
+++ js/oop_javascrpt.js
@@ -1,10 +1,10 @@
 var soy = "Hello Indonesia";
 var kai = soy.trim(6, 14);
-console.log(kai);
+console.log(kai);       //Hello Indonesia
 
 var say = new String ("hello indo");
 var kar = say.trim(6, 2);
-console.log(kar);
+console.log(kar);       //hello indo
 
 
 class Car{
@@ -15,9 +15,21 @@ class Car{
     }
 }
 var carAndi = new Car( "Toyota Innova", "MPV", 175000000);
-console.log(carAndi.merk);
-console.log(carAndi.price);
+console.log(carAndi.merk);       //Toyota Innova
+console.log(carAndi.price);      //175000000
 
 var carJoko = new Car( "Audi A8", "Sport", 455000000);
-console.log(carJoko.merk);
-console.log(carJoko.price);
+console.log(carJoko.merk);      //Audi A8
+console.log(carJoko.price);     //455000000
+
+//CLASS PROPERTY
+class carBudi {
+    constructor (){
+        this.merk1 = "Daihatsu Xenia";
+        this.type1 = "MPV";
+        this.price1 - 239000000;
+    }
+}
+
+var carBudiX = new carBudi();
+console.log(carBudiX);     //carBudi {merk1 : "Daihatsu Xenia", type1: "MPV" }
