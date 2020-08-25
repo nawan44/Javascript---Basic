292facdf74c064afbcbad867b40ce80a447f6090
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-25 23:50:32 +0700

    String Object

---
+++ js/string_object.js
@@ -0,0 +1,12 @@
+var AAA = new String("Learn Javascript");
+console.log(AAA);
+
+console.log(typeof AAA);
+console.log(AAA.length);
+console.log(AAA.toUpperCase);
+
+var ABA = "Learn Javascript";
+console.log(ABA);
+console.log(typeof ABA);
+console.log(ABA.length);
+console.log(ABA.toUpperCase);
