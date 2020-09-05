12e2d56ce781c979cfc40443ea411538ad6af8ca
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-05 19:08:32 +0700

    string object all

--- js/string_object.js
+++ js/string_object.js
@@ -83,4 +83,184 @@ console.log(z)
 
 const a = { x: 1, y: 2 }; const b = 'y'; const c = a[b]; console.log(c); 
 
+//Method String.prototype.substr()
+var AIA = "Barcelona Football Club";
+console.log(AIA);   //Barcelona Football Club
+console.log(AIA.substr(1));    //arcelona Football Club
+console.log(AIA.substr(-2));   //ub
+console.log(AIA.substr(-3));   //lub
+console.log(AIA.substr(4));    //elona Football Club
+console.log(AIA.substr(3, 4));   //celo
+console.log(AIA.substr(-3, 4));   //lub
+
+
+//Method String.prototype.substring()
+console.log(AIA.substring(1));    //arcelona Football Club
+console.log(AIA.substring(-2));   //Barcelona Football Club
+console.log(AIA.substring(-3));   //Barcelona Football Club
+console.log(AIA.substring(4));    //elona Football Club
+console.log(AIA.substring(3, 4));  //c
+console.log(AIA.substring(-3, 4));   //Barc
+
+
+//Method String.prototype.slice()
+console.log(AIA.slice(1));    //arcelona Football Club
+console.log(AIA.slice(0, -22));    //B
+console.log(AIA.slice(-2));    //ub
+console.log(AIA.slice(-3));     //lub
+console.log(AIA.slice(3));      //celona Football Club
+console.log(AIA.slice(3,4));     //c
+console.log(AIA.slice(3, -4));   //celona Football 
+
+//Method String.prototype.split()
+console.log(AIA.split());   //["Barcelona Football Club"]
+console.log(AIA.split(''));  //["B", "a", "r", "c", "e", "l", "o", "n", "a", " ", "F", "o", "o", "t", "b", "a", "l", "l", " ", "C", "l", "u", "b"]
+console.log(AIA.split('o'));   //["Barcel", "na F", "", "tball Club"]
+console.log(AIA.split('a'));   //["B", "rcelon", " Footb", "ll Club"]
+
+var AJA = "one two three four five six ";
+console.log(AJA.split(' ', 4));   //["one", "two", "three", "four"]
+
+var AKA = "one,two;three,four-five+six";
+console.log(AKA.split(/\W/))   //["one", "two", "three", "four", "five", "six"]
+
+//Method String.prototype.trim()
+
+var ALA = "    Liverpool Football Club    ";
+console.log(ALA.length);   //31
+console.log(ALA);   //    Liverpool Football Club    
+
+ALA = ALA.trim();
+console.log(ALA.length);  //23
+console.log(ALA);   //Liverpool Football Club
+
+
+var username = "admin ";
+if(username === "admin") {
+    console.log("Welcome admin...")
+}
+else{
+    console.log("User Not Found");
+}
+//User Not Found
+
+
+//Method String.prototype.concat()
+var AMA = "Manchester";
+var ANA = AMA.concat(" United");
+console.log(ANA);   //Manchester United
+
+console.log(ANA.concat(" Old Traffod"));   //Manchester United Old Traffod
+
+//Method String.prototype.includes()
+
+var AOA = "Real Madrid Santiago Bernebau";
+console.log(AOA.includes('Madrid'));     //true
+console.log(AOA.includes('madrid'));     //false
+console.log(AOA.includes('Real', 1));    //false
+console.log(AOA.includes('eal', 1));     //true
+console.log(AOA.includes('Real', 0));    //true
+console.log(AOA.includes('al', 2));     //true
+
+//Method String.prototype.startsWith() String.prototype.endsWith()
+
+console.log(AOA);                        //Real Madrid Santiago Bernebau
+console.log(AOA.startsWith('Real'));     //true
+console.log(AOA.startsWith('Madrid'));   //false
+console.log(AOA.startsWith('real'));     //false
+console.log(AOA.startsWith('madrid'));   //false
+
+console.log(AOA.endsWith('Bernebau'));     //true
+console.log(AOA.endsWith('bernebau'));     //false
+
+console.log(AOA.startsWith('Madrid', 5));   //true
+console.log(AOA.endsWith('Madrid', 11));    //true
+console.log(AOA.endsWith('Madrid', 10));    //false
+
+//Method String.prototype.repeat()
+
+// console.log(AOA.repeat(-1));
+console.log(AOA.repeat(0));    //
+console.log(AOA.repeat(1));    //Real Madrid Santiago Bernebau
+console.log(AOA.repeat(5));     //Real Madrid Santiago BernebauReal Madrid Santiago BernebauReal Madrid Santiago BernebauReal Madrid Santiago BernebauReal Madrid Santiago Bernebau
+console.log('Stadium'.repeat(7));    //StadiumStadiumStadiumStadiumStadiumStadiumStadium
+
+
+//Method String.prototype.toString() String.prototype.valueOf()
+var APA = new String ("West Java");
+console.log(APA);           //String {"West Java"}
+console.log(typeof APA);    //object
+
+AQA = APA.toString();
+console.log(typeof AQA);    //string
+console.log(AQA);           //West Java
+
+var ARA = APA.valueOf();    
+console.log(typeof ARA);    //string
+console.log(ARA);           //West Java
+
+//Method String.prototype.indexOf()
+console.log(AOA);                //Real Madrid Santiago Bernebau
+console.log(AOA.indexOf('Madrid'));       //5
+console.log(AOA.indexOf('madrid'));       //-1 {not found}
+console.log(AOA.indexOf('Santiago'));     //12
+
+var count = 0;
+var position = AOA.indexOf('a');
+
+while(position !==-1){
+    count++;
+    position = AOA.indexOf('a', position +1);
+}
+console.log(count);    //5
+
+//Method String.prototype.lastIndexOf()
+console.log(AOA);       //Real Madrid Santiago Bernebau
+console.log(AOA.lastIndexOf('Real'));     //0
+console.log(AOA.lastIndexOf('Madrid'));     //5
+console.log(AOA.lastIndexOf('madrid'));     //-1 {not found}
+console.log(AOA.lastIndexOf('Santiago'));     //12
+console.log(AOA.lastIndexOf('Bernebau'));     //21
+
+console.log(AOA.lastIndexOf('Madrid', 5));   //5
+console.log(AOA.lastIndexOf('Madrid', 3));   //-1
+
+var count1 = 0;
+var position1 = AOA.lastIndexOf('e');
+
+while(position1 !==-1){
+    count1++;
+    position1 = AOA.lastIndexOf('e', position1 -1);
+}
+
+console.log(count1);   //3
+
+//Method String.prototype.search()
+console.log(AOA.search('Madrid'));   //5
+console.log(AOA.search('madrid'));   //-1
+console.log(AOA.search(/madrid/i));   //5
+console.log(AOA.search('Santiago'));   //12
+
+//String.prototype.match()
+console.log(AOA.match('Madrid'));     //Array(1)
+console.log(AOA.match('madrid'));     //null
+console.log(AOA.match('a'));     //Array(1)
+console.log(AOA.match(/madrid/));     //null
+console.log(AOA.match(/madrid/i));     //Array(1)
+console.log(AOA.match('Bar'));     //null
+
+var ASA = "1 = 60 minute = 3600 secomf";
+console.log ( ASA.match(/\d+/g));  //["1", "60", "3600"]
+
+//Method String.prototype.replace()
+console.log(AOA);   //Real Madrid Santiago Bernebau
+console.log(AOA.replace("Santiago Bernebau", "Camp Nou"));   //Real Madrid Camp Nou
+console.log ( AOA.replace(/a/g,"Q") );    //ReQl MQdrid SQntiQgo BernebQu
+
+
+
+
+
+
+
 
