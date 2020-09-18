ea0d32d4dbcbc66f9bc3e81da8504a310dc9ed29
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-18 22:54:57 +0700

    Element Object {by Id, by Tag name}

---
+++ js/element_object.js
@@ -0,0 +1,9 @@
+// //Method document.getElementById()
+// var aaa = document.getElementById("emTag");
+// console.log(aaa.nodeName);
+// console.log(aaa.nodeType);
+// console.log(aaa.childNodes);
+// console.log(aaa.textContent);
+
+// aaa.textContent = "Document Object"
+
